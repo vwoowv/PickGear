57ba83afb62f8b95475e531d843ad6f34e5ddb84
vwoowv <vwoowv@dottwo.io>
2025-08-16 17:14:21 +0900

    [Game] 게임 구성 추가

--- CatchEggs/assets/scripts/egg.ts
+++ CatchEggs/assets/scripts/egg.ts
@@ -1,14 +1,18 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, Sprite } from 'cc';
+import { EggType } from './gameDefine';
 const { ccclass, property } = _decorator;
 
 @ccclass('egg')
 export class egg extends Component {
-    start() {
+    @property(Sprite)
+    private eggImage: Sprite = null;
+
+    public initialize(egg: EggType) {
 
     }
 
     update(deltaTime: number) {
-        
+
     }
 }
 
---
+++ CatchEggs/assets/scripts/gameDefine.ts
@@ -0,0 +1,12 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+export enum EggType {
+    DoArin,
+    EmmaMoon,
+    Happy,
+    Howsam,
+    Hoyang,
+    SongUnbee,
+    SooHana,
+}
