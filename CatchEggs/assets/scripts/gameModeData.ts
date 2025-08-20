e383d02ddb190f00c5356e3ad29bb288b3720289
vwoowv <vwoowv@dottwo.io>
2025-08-20 22:46:31 +0900

    [Game] 팰리컨 설정

--- CatchEggs/assets/scripts/gameModeData.ts
+++ CatchEggs/assets/scripts/gameModeData.ts
@@ -9,23 +9,33 @@ export class gameModeData extends Component {
     private gameManagerInstance: gameManager = null;
     @property({ type: Enum(EGameMode) })
     private currentGameMode: EGameMode = EGameMode.Version1;
+    @property(Node)
+    private enemyParent: Node[] = [];
     private backgroundName: string[] = ["background", "backgroundLake", "backgroundCity"];
 
     private onTouchVersion1Button() {
         this.currentGameMode = EGameMode.Version1;
-        this.gameManagerInstance = this.node.getComponent(gameManager)
-        this.gameManagerInstance.completeSelectGameMode();
+        this.completeSelectGameMode();
     }
 
     private onTouchVersion2Button() {
         this.currentGameMode = EGameMode.Version2;
-        this.gameManagerInstance = this.node.getComponent(gameManager)
-        this.gameManagerInstance.completeSelectGameMode();
+        this.completeSelectGameMode();
     }
 
     private onTouchVersion3Button() {
         this.currentGameMode = EGameMode.Version3;
-        this.gameManagerInstance = this.node.getComponent(gameManager)
+        this.completeSelectGameMode();
+    }
+
+    private completeSelectGameMode() {
+        this.enemyParent.forEach(enemy => {
+            enemy.active = false;
+        });
+        this.enemyParent[this.currentGameMode].active = true;
+        if (this.gameManagerInstance == null) {
+            this.gameManagerInstance = this.node.getComponent(gameManager)
+        }
         this.gameManagerInstance.completeSelectGameMode();
     }
 
