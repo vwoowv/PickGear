ff8e4bf8c0ad88f7856c34afe1340c3fe69d973c
vwoowv <vwoowv@dottwo.io>
2025-08-22 00:24:12 +0900

    [Game] 스코어 추가

---
+++ CatchEggs/assets/scripts/eggScore.ts
@@ -0,0 +1,16 @@
+import { _decorator, AnimationComponent, Component, RichText } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('eggScore')
+export class eggScore extends Component {
+    @property(RichText)
+    private scoreText: RichText = null;
+    @property(AnimationComponent)
+    private scoreAnimation: AnimationComponent = null;
+
+    public setScore(score: number) {
+        this.scoreText.string = score.toString();
+        this.scoreAnimation.play("idle");
+    }
+}
+
