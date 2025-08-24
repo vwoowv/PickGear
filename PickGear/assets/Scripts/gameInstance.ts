b9c9598a5ebe62827a06761815910c446c730bfe
vwoowv <vwoowv@dottwo.io>
2025-08-24 16:58:59 +0900

    [Character] 캐릭터 로드 확인

--- PickGear/assets/Scripts/gameInstance.ts
+++ PickGear/assets/Scripts/gameInstance.ts
@@ -9,6 +9,8 @@ const { ccclass, property } = _decorator;
 export class gameInstance extends Component {
     @property(Node)
     private dancerPos: Node = null;
+    @property(Node)
+    private dancerResultPos: Node[] = [];
 
     start() {
 
@@ -21,7 +23,16 @@ export class gameInstance extends Component {
     public async onStartButtonClick() {
         // 캐릭터의 기본적인 구성을 먼저 맞춰놓는다
         console.log("onStartButtonClick");
-        const newDancer = await ResourceManager.I.spawnPrefab<dancer>("prefab/character/Dancer", this.dancerPos);
-        newDancer.initialize(ECharacterType.EmmaMoon);
+        const newDancer1 = await ResourceManager.I.spawnPrefab<dancer>("prefab/character/Dancer", this.dancerResultPos[0]);
+        newDancer1.initialize(ECharacterType.DoArin);
+
+        const newDancer2 = await ResourceManager.I.spawnPrefab<dancer>("prefab/character/Dancer", this.dancerResultPos[1]);
+        newDancer2.initialize(ECharacterType.EmmaMoon);
+
+        const newDancer3 = await ResourceManager.I.spawnPrefab<dancer>("prefab/character/Dancer", this.dancerResultPos[2]);
+        newDancer3.initialize(ECharacterType.SongUnbee);
+
+        const newDancer4 = await ResourceManager.I.spawnPrefab<dancer>("prefab/character/Dancer", this.dancerResultPos[3]);
+        newDancer4.initialize(ECharacterType.SooHana);
     }
 }
