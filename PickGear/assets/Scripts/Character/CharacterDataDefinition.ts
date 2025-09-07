c534d33e99c76101b40115a6abc081b4814f95bb
vwoowv <vwoowv@dottwo.io>
2025-09-07 17:20:03 +0900

    [Refactoring]

--- PickGear/assets/Scripts/Character/CharacterDataDefinition.ts
+++
@@ -1,9 +0,0 @@
-export class CharacterDataDefinition {
-    public characterName: string;
-    public characterSpriteFramePath: string;
-
-    constructor(characterName: string, characterSpriteFramePath: string) {
-        this.characterName = characterName;
-        this.characterSpriteFramePath = characterSpriteFramePath;
-    }
-}
