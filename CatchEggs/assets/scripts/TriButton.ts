4fb0a210e7730a28a3f65ea76b2f1742c5d1fc5d
vwoowv <vwoowv@dottwo.io>
2025-12-12 21:25:00 +0900

    [Game] 선택 화면 변경

---
+++ CatchEggs/assets/scripts/TriButton.ts
@@ -0,0 +1,102 @@
+import { _decorator, Component, Node, input, Input, EventTouch, Vec3, PhysicsSystem2D, PolygonCollider2D, UITransform, Camera, Vec2, EventHandler, Button } from 'cc';
+
+const { ccclass, property, requireComponent } = _decorator;
+
+@ccclass('TriButton')
+@requireComponent(PolygonCollider2D) // 이 컴포넌트가 필수로 필요함을 명시
+export class TriButton extends Component {
+
+    @property(Camera)
+    uiCamera: Camera = null; // 2D UI용 카메라를 연결해주세요.
+
+    @property({ type: EventHandler })
+    clickEvents: EventHandler[] = []; // 클릭 이벤트 핸들러 배열
+
+    private collider: PolygonCollider2D = null;
+    private uiTransform: UITransform = null;
+    private originalScale: Vec3 = null;
+    private isPressed: boolean = false;
+
+    onLoad() {
+        this.collider = this.getComponent(PolygonCollider2D);
+        this.uiTransform = this.getComponent(UITransform);
+        this.originalScale = this.node.scale.clone();
+    }
+
+    onEnable() {
+        // 터치(마우스 클릭) 시작 이벤트를 등록합니다.
+        input.on(Input.EventType.TOUCH_START, this.onTouchStart, this);
+        input.on(Input.EventType.TOUCH_END, this.onTouchEnd, this);
+        input.on(Input.EventType.TOUCH_CANCEL, this.onTouchCancel, this);
+    }
+
+    onDisable() {
+        // 컴포넌트가 비활성화될 때 이벤트를 제거합니다. (메모리 누수 방지)
+        input.off(Input.EventType.TOUCH_START, this.onTouchStart, this);
+        input.off(Input.EventType.TOUCH_END, this.onTouchEnd, this);
+        input.off(Input.EventType.TOUCH_CANCEL, this.onTouchCancel, this);
+    }
+
+    private onTouchStart(event: EventTouch) {
+        if (!this.uiCamera) {
+            console.warn("UI Camera is not set!");
+            return;
+        }
+
+        // 화면 터치 좌표를 가져옵니다. getLocation은 좌하단 기준 좌표입니다.
+        const touchLocation = event.getLocation();
+
+        // 월드 좌표로 변환합니다.
+        const worldPoint = this.uiCamera.screenToWorld(new Vec3(touchLocation.x, touchLocation.y, 0));
+        
+        // 월드 좌표에 있는 콜라이더들을 가져옵니다.
+        const hitColliders = PhysicsSystem2D.instance.testPoint(new Vec2(worldPoint.x, worldPoint.y));
+
+        // 이 노드의 콜라이더가 포함되어 있는지 확인합니다.
+        if (hitColliders.some(c => c.uuid === this.collider.uuid)) {
+            this.isPressed = true;
+            this.node.setScale(this.originalScale.x * 0.9, this.originalScale.y * 0.9, this.originalScale.z);
+
+            console.log("삼각형 버튼이 클릭되었습니다!");
+            this.node.emit('TriButton_clicked');
+        }
+    }
+
+    private onTouchCancel(event: EventTouch) {
+        if (this.isPressed) {
+            this.node.setScale(this.originalScale);
+            this.isPressed = false;
+        }
+    }
+
+    private onTouchEnd(event: EventTouch) {
+        if (this.isPressed) {
+            this.node.setScale(this.originalScale);
+            this.isPressed = false;
+        }
+
+        if (!this.uiCamera) {
+            console.warn("UI Camera is not set!");
+            return;
+        }
+
+        // 화면 터치 좌표를 가져옵니다. getLocation은 좌하단 기준 좌표입니다.
+        const touchLocation = event.getLocation();
+
+        // 월드 좌표로 변환합니다.
+        const worldPoint = this.uiCamera.screenToWorld(new Vec3(touchLocation.x, touchLocation.y, 0));
+
+        // 월드 좌표에 있는 콜라이더들을 가져옵니다.
+        const hitColliders = PhysicsSystem2D.instance.testPoint(new Vec2(worldPoint.x, worldPoint.y));
+
+        // 이 노드의 콜라이더가 포함되어 있는지 확인합니다.
+        if (hitColliders.some(c => c.uuid === this.collider.uuid)) {
+            console.log("삼각형 버튼이 터치 종료되었습니다!");
+
+            this.clickEvents.forEach(eventHandler => {
+                eventHandler.emit([]);
+            });
+        }
+        this.node.emit('TriButton_touchEnd');
+    }
+}
